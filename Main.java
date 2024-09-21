970da4133ba1def405af36e02bca72ab489357ad
Mahima-Prajapati <118904414+Mahima-Prajapati@users.noreply.github.com>
2024-09-21 13:49:24 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+   
+	// Method to create a player based on user input
+    public static Player createPlayer(char player){
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user to enter name for Player
+        System.out.println("Enter name of Player " + player);
+        String name = scanner.nextLine();
+
+        // Prompt user to enter attributes for Player
+        System.out.println("Enter attributes for Player " + player);
+        int health = scanner.nextInt();
+        int strength = scanner.nextInt();
+        int attack = scanner.nextInt();
+
+        scanner.close();
+    	
+        // Returning a new Player with the provided attributes
+        return new Player(name, health, strength, attack);
+    }
+    // Main method (entry of program)
+    public static void main(String[] agrs){
+        // Creating two players
+        Player playerA = createPlayer('A');
+        Player playerB = createPlayer('B');
+        
+        // Creating a Game with the above players
+        Game game = new Game(playerA, playerB);
+
+        // Starting a Game using start method from Game's class
+        game.start();
+    }
+}
