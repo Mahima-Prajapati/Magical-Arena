c9e2c7282c1ab203784d5463f74ecd2da134d837
Mahima-Prajapati <118904414+Mahima-Prajapati@users.noreply.github.com>
2025-01-04 21:58:22 +0530

    Added Main.java

--- Main.java
+++ Main.java
@@ -32,9 +32,8 @@ public class Main {
         Player playerA = createPlayer('A');
         Player playerB = createPlayer('B');
 
-        System.out.println("My name is Mahima");
-        System.out.println("My name is Prajapati");
-        
+        System.out.println("This is remote main branch");
+
         // Creating a Game with the above players
         Game game = new Game(playerA, playerB);
 
