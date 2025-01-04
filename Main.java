3bfd39d1093aec8db065e97438ad3719ea621527
Mahima-Prajapati <118904414+Mahima-Prajapati@users.noreply.github.com>
2025-01-04 16:25:52 +0000

    Committing

--- Main.java
+++ Main.java
@@ -32,8 +32,7 @@ public class Main {
         Player playerA = createPlayer('A');
         Player playerB = createPlayer('B');
 
-        System.out.println("My name is Mahima");
-        System.out.println("My name is Prajapati");
+        System.out.println("This is My Local Branch");
         
         // Creating a Game with the above players
         Game game = new Game(playerA, playerB);
